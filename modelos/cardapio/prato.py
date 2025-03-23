3b9840a3b14ab5491bf32172f76dca3949947208
miguelsett <miguelsettimio@gmail.com>
2025-03-23 19:16:00 -0300

    alterando classe Prato para herdar atributos da classe ItemCardapio

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,5 +1,6 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao):
-        self.nome = nome
-        self.preco = preco
+        super().__init__(nome, preco)
         self.descricao = descricao
