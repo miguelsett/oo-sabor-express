4ffb6425e50f8527a2dc3d69b803dcbc15a22159
unknown <miguelsettimio@gmail.com>
2025-03-23 15:11:16 -0300

    projeto inicial

---
+++ app.py
@@ -0,0 +1,16 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+
+restaurante_praca.receber_avaliacao('Gui', 5)
+restaurante_praca.receber_avaliacao('Lais', 4)
+restaurante_praca.receber_avaliacao('Emy', 1)
+
+print(restaurante_praca.media_avaliacoes)
+
+def main():
+    Restaurante.listar_restaurantes()
+
+
+if __name__ == '__main__':
+    main()
---
+++ modelos/avaliacao.py
@@ -0,0 +1,4 @@
+class Avaliacao:
+    def __init__(self, cliente, nota):
+        self._cliene = cliente
+        self._nota = nota
