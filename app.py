fdc9ddff543256e98afcd8c136af54ffd3328cad
miguelsett <miguelsettimio@gmail.com>
2025-03-23 19:30:02 -0300

    testando heranças do ItemCardapio

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 
-restaurante_praca.receber_avaliacao('Gui', 5)
-restaurante_praca.receber_avaliacao('Lais', 4)
-restaurante_praca.receber_avaliacao('Emy', 1)
-
-print(restaurante_praca.media_avaliacoes)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 
 if __name__ == '__main__':
