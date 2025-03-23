c03ec117ebf96c9edb951031e1a0ae81bc88026a
miguelsett <miguelsettimio@gmail.com>
2025-03-23 20:29:02 -0300

    adicionando método abstrato nas classes filhas de ItemCardapio

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -4,3 +4,6 @@ class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao):
         super().__init__(nome, preco)
         self.descricao = descricao
+
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.08)
